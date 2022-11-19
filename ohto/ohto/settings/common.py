f6f2e1c6e76b775563e90df398917a1a5c57483e
GoGangH <rhrkdgus1020@naver.com>
2022-11-20 02:29:38 +0900

    경로 변경 테스트

--- ohto/ohto/settings/common.py
+++ ohto/ohto/settings/common.py
@@ -28,7 +28,10 @@ SECRET_KEY = 'django-insecure-v6w_8s=)_(rbclexof0=r$4epuwdm$x#wd77v$3i!0+4(q^b0x
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [
+    '.ec2-43-200-92-0.ap-northeast-2.compute.amazonaws.com',
+    '.ohto.kr',
+]
 
 
 # Application definition
