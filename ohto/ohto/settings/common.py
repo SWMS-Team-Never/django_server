fa98b38dcd5df1371f85f5b81ef582a6e2e93721
GoGangH <rhrkdgus1020@naver.com>
2022-11-20 16:09:04 +0900

    uwsgi test

--- ohto/ohto/settings/common.py
+++ ohto/ohto/settings/common.py
@@ -29,7 +29,10 @@ SECRET_KEY = 'django-insecure-v6w_8s=)_(rbclexof0=r$4epuwdm$x#wd77v$3i!0+4(q^b0x
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    '*'
+    'ohto.kr',
+    '43.200.92.0',
+    'localhost',
+    '127.0.0.1'
 ]
 
 
